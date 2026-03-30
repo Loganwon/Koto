7e105fc410648614f9004e277cec4923602eb66c
Loganwon <a0911013943@gmail.com>
2026-03-30 12:03:25 +0800

    chore: remove temporary test/debug/log files from repo

--- _fix_spec3.py
+++
@@ -1,9 +0,0 @@
-import re
-
-with open('koto.spec', 'r', encoding='utf-8') as f:
-    text = f.read()
-
-text = text.replace("'web.app',", "'web', 'web.app',")
-
-with open('koto.spec', 'w', encoding='utf-8') as f:
-    f.write(text)
--- _tmpc.py
+++
@@ -1,7 +0,0 @@
-﻿with open('web/static/vendor/univer/preset-sheets-core.umd.js', encoding='utf-8', errors='replace') as f:
-    f.seek(0, 2)
-    size = f.tell()
-    f.seek(max(0, size-2000))
-    tail = f.read(2000)
-print('Last 2000 chars of preset-sheets-core.umd.js:')
-print(tail[-1500:])
